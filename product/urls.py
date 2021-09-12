104df529f0295d877126d0d464751b10fcb90d22
shahrvin <shervinmihan@gmail.com>
2021-09-13 02:11:53 +0430

    add to cart view

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('<int:pk>', DetailProduct.as_view(), name="detail")
+	path('<int:pk>', DetailProduct.as_view(), name="detail"),
+	path('add/<int:product_id>', add_to_cart, name="addtocart"),
+	path('cart/', add_to_cart, name="cart"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,12 +1,29 @@
-from django.views.generic import ListView , DetailView
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
 
-from product.models import Product
+from product.models import Product, Cart, CartItem
 
 
 class Home(ListView):
-	queryset = Product.objects.all()[:12]
-	template_name = "index.html"
+    queryset = Product.objects.all()[:12]
+    template_name = "index.html"
+
 
 class DetailProduct(DetailView):
-	model = Product
-	template_name = "detail.html"
+    model = Product
+    template_name = "detail.html"
+
+
+@login_required
+def add_to_cart(request, product_id):
+    product = Product.objects.get(product_id=product_id)
+    if product:
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        cartitem, itemcreated = CartItem.objects.get_or_create(product=product, cart=cart, price=product.price)
+        cartitem.quantity+=1
+        cartitem.save()
+        messages.success(request,"cart updated")
+        return redirect('detail',product_id)
+
